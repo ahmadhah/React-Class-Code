18e4bd20bbcdf0b0e39aee60bd0f70b2b5841ec5
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-08-17 12:14:33 +0500

    Hello world

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const title = React.createElement('h1', null, 'Ahmad Hannan')
+const secondElement = React.createElement('p', null, 'My Paragraph')
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    secondElement,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
