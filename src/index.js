f5a3b9cc368fbb20cc0ee55e673490308e73c1a6
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-08-17 12:16:27 +0500

    .

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ import ReactDOM from 'react-dom';
 const title = React.createElement('h1', null, 'Ahmad Hannan')
 const secondElement = React.createElement('p', null, 'My Paragraph')
 
+//? Rendering on DOM
+
 ReactDOM.render(
     secondElement,
     document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
