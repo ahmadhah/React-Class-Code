561d7c06c60ca31dfb54692dca8a22c53ba1f71d
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-08-20 12:15:11 +0500

    class three code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import logo from './Screenshot_2.png';
 import './App.css';
 
+// ! 1.scripts
+// ! 2.Build
+// ! 3. import style file in our component
+// ! image use
+// ! simple elements ko return
+// ! variable
+// ! object use
+// ! array with .map method
+
 function App() {
+
+
+  const list = [
+    {
+      title: 'React',
+      url: 'https://facebook.github.io/react/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://github.com/reactjs/redux',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  const name = "Ali"
+
+  const obj = {
+    a: "ahmad"
+  }
+  obj.b = "hannan"
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img className="image" src={logo}></img>
+      <h1>this is my image</h1>
+      <p>this is my paragraph</p>
+      <h2> My name is {name}</h2>
+      <h2> My name is {obj.a}{obj.b}</h2>
+      {list.map((item)=><h1>Title: {item.title}</h1>)}
     </div>
   );
 }
