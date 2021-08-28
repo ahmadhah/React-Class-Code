60d4c84d262aa3d1363d84b38b81cca251b08e8a
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-08-28 19:18:39 +0500

    continue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import ClassComponent from './ClassComponent'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ClassComponent a={123}/>
-    </div>
-  );
-}
+function App() {
+    let a = 4444;
+    function Ca(){
+        a = 999;
+      alert(a);
+    }
+  console.log(a);
+    return (
+      <div className="App">
+          <h1>{a}</h1>
+        <button onClick={Ca}>BTN</button>
+      </div>
+    );
+  }
+  
 
 export default App;
