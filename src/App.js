178d13dfb129cb0e1f0e0435b023abd934cbb216
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-08-28 14:42:28 +0500

    f

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import './App.css';
+import React from "react";
+// import "./App.css";
 import ClassComponent from './ClassComponent'
-
-
 function App() {
-  const name = "ahmad"
-  const email = "xyz@gmail.com"
-  const showName = () => {
+ const showName = ()=>{
     alert(name)
   }
-  // showName(name)
-  const object = { name, email, showName }
-  const array = ["Lahore", "FSD", "SKP", "Multan", { object }]
+    const name = "Areej Amjad"
+  const email ="areejamjad@gmail.com"
+  const object = {name,email,showName}
+  const array = ["Lahore", "FSD" , "SKP", "Multan",{object}]
 
+  
 
   return (
     <div className="App">
-      <ClassComponent arr={array} />
-    </div>
+   <ClassComponent  arr={array} />
+     </div>
   );
 }
+    
+  
 
 export default App;
-
--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -1,15 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class ClassComponent extends Component {
-
     render() {
+        // complete Array destructure + Object rename
+
+        const {
+            arr: [a, b, c, d, { object: ahmad }],
+        } = this.props;
+        const { name, email, showName } = ahmad;
+    
+        const cities = [a, b, c, d];
+
+        console.log(this.props);
 
         return (
-            <div>
-
+            <div style={{ textAlign: "center" }}>
+                <h2>
+                    Cities are
+                    <br />
+                    {cities.map((city,i) => <p key={i}>{city}</p>  )}
+                </h2>
+                <h4>
+                    Name is {name} <br />
+                    and
+                    <br /> email is {email}
+                </h4>
+                <button className="btn btn-primary" onClick={showName}>
+                    Click
+                </button>
             </div>
-        )
+        );
     }
 }
 
-export default ClassComponent
+export default ClassComponent;
