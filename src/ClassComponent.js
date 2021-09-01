1196fd96baac8e3f9ad91770d2914503c135f6fb
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-09-01 11:01:31 +0500

    mid

--- src/ClassComponent.js
+++ src/ClassComponent.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
-
 export class ClassComponent extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0,
+            name:'ahmad'
+        }
+        this.name = props.a;
+    }
+
     render() {
+
         return (
-            <div>
-                
+            <div style={{textAlign: 'center'}}>
+                <h1>Class Component</h1>
+                <h4>{this.state.count}</h4>
+                <h4>{this.state.name}</h4>
+                <button onClick={()=>this.setState({count:this.state.count+1,name:"Hannan"})}>Incre</button>
             </div>
         )
     }
