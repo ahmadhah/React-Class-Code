8eb23506cf5ff84cf5d47c8162cf00612fe84f7a
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-09-15 17:56:54 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-
-
-import React from 'react'
+import React, { useState } from 'react';
 import User from './Rough.js'
 function App() {
   // const [name, setName] = React.useState("Anil")
-
+  const [count, setcount] = useState(0);
   return (
     <div className="App">
       <h1>Render Method in React</h1>
-      <User />
+      <button onClick={() => setcount(count + 1)}>BTN</button>
+      <User count={count} />
       {/* <button onClick={()=>setName("Sidhu")}>Update Name</button> */}
     </div>
   );
--- src/Rough.js
+++ src/Rough.js
@@ -23,31 +23,76 @@
 // export default User;
 
 
-import { Component } from 'react'
+// import { Component } from 'react'
 
-export default class ClassC extends Component {
+// export default class ClassC extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-            data: 'Ahmad'
-        }
-        console.log('constructor');
-    }
-    componentDidMount() {
-        console.log("component did mount")
-    }
+//     constructor() {
+//         super();
+//         this.state = {
+//             data: 'Ahmad'
+//         }
+//         console.log('constructor');
+//     }
+//     componentDidMount() {
+//         console.log("component did mount")
+//     }
 
-    render() {
-        console.log("render");
+//     render() {
+//         console.log("render");
 
-        return (
-            <div>
-                <h1>Hello {this.state.data}</h1>
-                <button onClick={() => this.setState({ data: "Hannan" })}>brn2</button>
-            </div>
-        )
-    }
-}
+//         return (
+//             <div>
+//                 <h1>Hello {this.state.data}</h1>
+//                 <button onClick={() => this.setState({ data: "Hannan" })}>brn2</button>
+//             </div>
+//         )
+//     }
+// }
+
+
+
+//! component did update
+
+
+// import { Component } from 'react'
+
+// export default class ClassC extends Component {
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//             data: 'Ahmad'
+//         }
+//         console.log('i am constructor', props.count);
+//     }
+
+//     componentDidUpdate(preProps, preState, Snapshot) {
+//         if (preState.data === this.state.data) {
+//             console.log("both state are same")
+//         }
+//         console.log("Component did update ==> ", preState)
+//         console.log("previous props ", preProps);
+//         console.log('this is my count', this.props.count);
+//     }
+
+//     componentDidMount() {
+//         console.log("component did mount")
+//     }
+
+//     render() {
+//         console.log("render");
+
+//         return (
+//             <div>
+//                 <h1>Hello {this.state.data}</h1>
+//                 <button onClick={() => this.setState({ data: "Hannan" })}>brn2</button>
+//             </div>
+//         )
+//     }
+// }
+
+
+
 
 
