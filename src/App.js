da59897a7474a1ee21596e721807aacf90ff9ce9
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-09-28 09:23:11 +0500

    completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+//* Code 1
 
+//  App.js
+
+import { useState } from 'react';
+import './App.css';
+import CMP from './CMP'
 function App() {
+
+  const [ab, setab] = useState(1)
+  const [jk, setjk] = useState(4)
+  const [flag, setflag] = useState(true);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <User ab={ab} jk={jk} />
+      {flag ? <CMP /> : ''}
+      <button onClick={() => setab(ab + 1)}>btn</button>
+      <button onClick={() => setjk(jk + 1)}>btn2</button>
+      <button onClick={() => setflag(!flag)}>Flag</button>
     </div>
   );
 }
 
 export default App;
+
+// User.js
+
+import { useEffect } from 'react';
+
+function User(props) {
+
+  useEffect(() => {
+    console.log("component did mount")
+  }, []);
+  useEffect(() => {
+    console.log('componentDidUpdate')
+  });
+
+  useEffect(() => {
+    console.log("work 1")
+  }, [props.ab, props.jk])  //Note: 2no states k sth same useEffect chlany k liye hm likh skty [props.ab, props.jk]
+
+
+  return (
+    <div className="App">
+      <h1>{props.ab}</h1><p>{props.jk}</p>
+    </div>
+  );
+}
+
+
+
+//* CMP component 
+
+import React, { useState, useEffect } from 'react'
+
+
+function CMP() {
+  // useEffect(() => {
+  //     console.log('me a chuka')
+  // }, []);
+
+  useEffect(() => {
+    return () => { console.log('component will unmount') }
+  }, []);
+
+  return (
+    <div>
+      cmp
+    </div>
+  )
+}
+
+export default CMP
