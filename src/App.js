6381ae85a7e04d103c2f13d57f177f7cd3310f2c
ahmadhah <54240600+ahmadhah@users.noreply.github.com>
2021-10-01 09:02:20 +0500

    ..

--- src/App.js
+++ src/App.js
@@ -1,3 +1,36 @@
+//code -1
+
+//useArray
+
+import { useState } from 'react'
+
+export function useArray() {
+
+  const [students, setstudents] = useState([
+
+    {
+      name: 'ahmad',
+      id: 123
+    },
+    {
+      name: 'arslan',
+      id: 34
+    },
+    {
+      name: 'shabir',
+      id: 54
+    }
+
+  ])
+
+
+
+  return ([students, setstudents])
+
+}
+
+//app.js
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -23,3 +56,98 @@ function App() {
 }
 
 export default App;
+import React from 'react'
+import { useArray } from './useArray';
+
+function App() {
+
+  const [students, setstudents] = useArray();
+  console.log(setstudents)
+
+  return (
+    <div>
+      {students.map((student, i) => <h2 key={i}>#{i} Name is : {student.name}</h2>)}
+    </div>
+  )
+}
+
+export default App
+
+
+
+
+
+//2nd example
+
+//////////////////useArray.js
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+export function useArray() {
+
+  const [products, setproducts] = useState([])
+  //    async function fetchProduct(){
+  //         let productResponse = await fetch('https://fakestoreapi.com/products/jewelery')
+  //         let products =await productResponse.json();
+  //         setproducts(products)
+  //         console.log(products)
+  //    }
+
+
+  // by using axios   (axios me .json() function call krny ki need nhi hoti)
+
+  async function fetchProduct() {
+    try {
+      let productss = await axios.get('https://fakestoreapi.com/products')
+      // let products =await productResponse.json();
+      setproducts(productss.data)
+      // console.log(productss.data)
+    }
+    catch (err) {
+      console.log('error ', err)
+    }
+    // setproducts(products)
+
+  }
+
+
+  useEffect(() => {
+    fetchProduct();
+  }, [])
+
+
+  return ([products])
+
+}
+
+----------------------------------------------------
+
+///////////////////App.js
+
+
+import React from 'react'
+import { useArray } from './useArray';
+
+function App() {
+
+  const [products] = useArray();
+
+
+  return (
+    <div className="container">
+
+      {products.map((produc) => {
+        return (
+          < div style={{ marginLeft: "25%" }} key={produc.id}>
+            <img style={{ width: '150px' }} src={produc.image} alt={"men"} />
+            <p className="title">{produc.title}</p>
+            <h3 className="prize">{produc.price}</h3>
+            <br /><hr /><br />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default App
